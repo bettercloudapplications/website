ed05ac5c083c1213ada40c2e5a43be87e2d75f26
alechko <mail@alechko.net>
2022-09-18 13:00:02 +0300

    adding images and deploying.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'bettercloudapplications', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -77,7 +77,7 @@ const config = {
             position: 'right',
             items: [
               { label: 'Time Sheets', to: 'docs/timesheets' },
-              { label: 'Project Manager', to: 'docs/projectmanager' },
+              // { label: 'Project Manager', to: 'docs/projectmanager' },
             ],
           },
         ],
